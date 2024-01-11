846c1f74dfa8b33e9bad123c958b5a160c969461
Juan Alvarez <alvarez.juanign@gmail.com>
2024-01-11 17:31:41 -0300

    feat: add logic for upload info to the database

--- appLicencias/src/App.jsx
+++ appLicencias/src/App.jsx
@@ -7,16 +7,36 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 function App () {
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const fields = Object.fromEntries(new FormData(e.target))
+    const { data, error } = await supabase
+      .from('docentes')
+      .insert([
+        {
+          nombre: `${fields.nombre}`,
+          licencia: `${fields.licencia}`,
+          desde: `${fields.desde}`,
+          hasta: `${fields.hasta}`,
+          observaciones: `${fields.observaciones}`
+        }
+      ])
+      .select()
+
+    if (error) console.error(error)
+    if (data) console.log(data)
+  }
+
   return (
     <main>
-      <h1>Hello</h1>
-      <form className='formularioLic'>
+      <h1>Licencias / Inasistencias</h1>
+      <form className='formularioLic' onSubmit={handleSubmit}>
         <input name='nombre' type='text' placeholder='Nombre y apellidos...' />
         <input name='licencia' type='text' placeholder='Licencia...' />
-        <input type='date' />
-        <input type='date' />
-        <input type='text' placeholder='Observaciones...' />
-        <button type='submit'>Buscar</button>
+        <input name='desde' type='date' />
+        <input name='hasta' type='date' />
+        <textarea name='observaciones' className='observaciones' placeholder='Observaciones...' />
+        <button type='submit'>Añadir licencia</button>
       </form>
     </main>
   )
