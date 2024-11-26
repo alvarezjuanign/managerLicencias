b7fd023662bb835c58043607128e6a49b5545166
Juan Alvarez <alvarez.juanign@gmail.com>
2024-11-26 00:46:55 -0300

    add styles with tailwind

--- licenciasApp/src/main.jsx
+++ licenciasApp/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { App } from './App.jsx'
+import { Planilla } from './Planilla.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Planilla />
+    <Planilla />
+    <Planilla />
+    <Planilla />
+    <footer className="text-center m-4 ">
+      <button
+        className="bg-green-500 text-white py-2 px-6 rounded-lg shadow hover:bg-green-600 @media print:hidden"
+        onClick={window.print}
+      >
+        Descargar
+      </button>
+    </footer>
   </StrictMode>,
 )
