967d2d09a09b19ba7d9db7ce3688f6428d3cc0c4
Juan Alvarez <alvarez.juanign@gmail.com>
2024-11-26 17:46:37 -0300

    refactoring index

--- licenciasApp/src/main.jsx
+++ licenciasApp/src/main.jsx
@@ -1,21 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { App } from './App.jsx'
 import './index.css'
-import { Planilla } from './Planilla.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Planilla />
-    <Planilla />
-    <Planilla />
-    <Planilla />
-    <footer className="text-center m-4 ">
-      <button
-        className="bg-green-500 text-white py-2 px-6 rounded-lg shadow hover:bg-green-600 @media print:hidden"
-        onClick={window.print}
-      >
-        Descargar
-      </button>
-    </footer>
-  </StrictMode>,
+  < StrictMode >
+    <App></App>
+  </StrictMode >
 )
