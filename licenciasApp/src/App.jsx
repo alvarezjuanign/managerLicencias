167a1159e6ddd975aaf2f5f1390ac7e94fc32496
Juan Alvarez <alvarez.juanign@gmail.com>
2025-01-09 22:04:02 -0300

    Add event on storage data

--- licenciasApp/src/App.jsx
+++ licenciasApp/src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import { Planilla } from './Components/Planilla.jsx'
 import { Formulario } from './Components/Formulario.jsx'
 import { ToastContainer, toast } from 'react-toastify'
+import { useEffect } from 'react'
 
 export function App() {
   const [activo, setActivo] = useState(false)
+  const [profesoras, setProfesoras] = useState(true)
 
   const handleDelete = () => {
     localStorage.removeItem("Profesoras")
@@ -18,31 +20,50 @@ export function App() {
       progress: undefined,
       theme: "dark",
     })
+
+    window.dispatchEvent(new Event("storage"))
   }
 
-  return (
-    <>
-      <button
-        onClick={() => setActivo(!activo)}
-        className="bg-amber-600 p-2 m-3 w-30 text-white hover:bg-yellow-500 focus:outline-none font-bold rounded-full text-base px-5 py-2.5 text-center me-2 mb-2"
-      >
-        {activo ? "Formulario" : "Planilla"}
-      </button>
+  useEffect(() => {
+    const revisarLocalStorage = () => {
+      const data = localStorage.getItem("Profesoras")
+      setProfesoras(!data)
+    }
+    revisarLocalStorage()
 
-      {
-        activo ?
-          <>
-            <button
-              onClick={handleDelete}
-              className="bg-amber-600 p-2 m-3 w-30 text-white hover:bg-yellow-500 focus:outline-none font-bold rounded-full text-base px-5 py-2.5 text-center me-2 mb-2"
-            >
-              Borrar datos
-            </button>
-            <Planilla></Planilla>
-            <ToastContainer />
-          </>
-          : <Formulario></Formulario>
-      }
-    </>
+    window.addEventListener("storage", revisarLocalStorage)
+
+    return () => {
+      window.removeEventListener("storage", revisarLocalStorage)
+    }
+  }, [])
+
+  return (
+    <main className="h-screen">
+      <nav>
+        <button
+          onClick={() => setActivo(!activo)}
+          className="absolute mt-4 ml-4 w-36 bg-[#B0D7FF] text-[#2D3142] border border-transparent px-5 py-2 rounded-full shadow-md hover:bg-[#EAE8FF] hover:text-[#ADACB5] active:bg-[#ADACB5] active:text-[#EAE8FF] focus:ring-2 focus:ring-[#B0D7FF] focus:outline-none transition-all duration-200"
+        >
+          {activo ? "Formulario" : "Planilla"}
+        </button>
+
+        {
+          activo ?
+            <>
+              <button
+                disabled={profesoras}
+                onClick={handleDelete}
+                className=" absolute left-40 mt-4 ml-4 w-36 bg-[#B0D7FF] text-[#2D3142] border border-transparent px-5 py-2 rounded-full shadow-md hover:bg-[#EAE8FF] hover:text-[#ADACB5] active:bg-[#ADACB5] active:text-[#EAE8FF] focus:ring-2 focus:ring-[#B0D7FF] focus:outline-none transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-[#B0D7FF] disabled:text-[#2D3142]"
+              >
+                Borrar datos
+              </button>
+              <Planilla></Planilla>
+              <ToastContainer />
+            </>
+            : <Formulario></Formulario>
+        }
+      </nav>
+    </main>
   )
 }
