49f31a04237a3f3ee42d89f0a45cff3845bc66d9
Juan Alvarez <alvarez.juanign@gmail.com>
2025-01-09 22:04:39 -0300

    Change style, add event for storage data

--- licenciasApp/src/Components/Formulario.jsx
+++ licenciasApp/src/Components/Formulario.jsx
@@ -39,6 +39,7 @@ export function Formulario() {
     notify()
 
     localStorage.setItem("Profesoras", JSON.stringify(profesoras))
+    window.dispatchEvent(new Event("storage"))
 
     e.target.nombre.value = ""
     e.target.cod.value = ""
@@ -47,27 +48,40 @@ export function Formulario() {
   }
 
   return (
-    <main className="p-4 flex flex-col items-center">
-      < h2 className="text-2xl underline font-bold mb-4 text-center" > Formulario de licencias</h2 >
-      <form className="flex flex-col border rounded-md p-5 w-1/2 mt-10" onSubmit={manejoDatos}>
-        <label htmlFor="nombre" className="block mt-6" >
-          Nombre:
-        </label>
-        <input type="text" name="nombre" id="nombre" className="border-b-[1px] active:outline-none focus:outline-none focus:border-b-slate-600" />
-        <label htmlFor="cod" className="block mt-6" >
-          Cod. Licencia:
-        </label>
-        <input type="text" name="cod" id="cod" className="border-b-[1px] active:outline-none focus:outline-none focus:border-b-slate-600" />
-        <label htmlFor="dia" className="block mt-6" >
-          Dia:
-        </label>
-        <input type="text" name="dia" id="dia" className="border-b-[1px] active:outline-none focus:outline-none focus:border-b-slate-600" />
-        <label htmlFor="mes" className="block mt-6" >
-          Mes:
-        </label>
-        <input type="text" name="mes" id="mes" className="border-b-[1px] active:outline-none focus:outline-none focus:border-b-slate-600" />
+    <main className="flex flex-col items-center justify-center h-screen m-0 p-0">
+      <h2 className="text-2xl underline font-bold mb-4 text-center" > Formulario de licencias</h2 >
+      <form className="flex flex-col border border-grey-400 rounded-md p-5 w-3/4 mt-10 shadow-xl lg:w-1/2" onSubmit={manejoDatos}>
+        <div className="md:grid grid-cols-2 gap-8 justify-items-center">
+          <p className="w-full">
+            <label htmlFor="nombre" className="block mt-4" >
+              Nombre:
+            </label>
+            <input type="text" name="nombre" id="nombre" className="w-full border-b border-gray-300 py-1 focus:border-b focus:border-blue-400 transition-colors focus:outline-none peer bg-inherit" placeholder="Maria, Miriam..." />
+          </p>
 
-        <button type="submit" className="bg-amber-600 p-2 m-3 w-full text-white hover:bg-yellow-500 focus:outline-none font-bold rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2">Enviar</button>
+          <p className="w-full mt-4">
+            <label htmlFor="cod" className="block" >
+              Cod. Licencia:
+            </label>
+            <input type="text" name="cod" id="cod" className="w-full border-b border-gray-300 py-1 focus:border-b focus:border-blue-400 transition-colors focus:outline-none peer bg-inherit" maxLength={6} />
+          </p>
+
+          <p className="w-full mt-4">
+            <label htmlFor="dia" className="block" >
+              Dia:
+            </label>
+            <input type="text" name="dia" id="dia" className="w-full border-b border-gray-300 py-1 focus:border-b focus:border-blue-400 transition-colors focus:outline-none peer bg-inherit" placeholder="1, 2, 3..." />
+          </p>
+
+          <p className="w-full mt-4">
+            <label htmlFor="mes" className="block" >
+              Mes:
+            </label>
+            <input type="text" name="mes" id="mes" className="w-full border-b border-gray-300 py-1 focus:border-b focus:border-blue-400 transition-colors focus:outline-none peer bg-inherit" placeholder="Enero, Febrero, Marzo..." />
+          </p>
+
+          <button type="submit" className="mt-10 w-full bg-[#B0D7FF] text-[#2D3142] border border-transparent px-5 py-2 rounded-full shadow-md hover:bg-[#EAE8FF] hover:text-[#ADACB5] active:bg-[#ADACB5] active:text-[#EAE8FF] focus:ring-2 focus:ring-[#B0D7FF] focus:outline-none transition-all duration-200 col-span-2">Enviar</button>
+        </div>
       </form>
 
       <ToastContainer
