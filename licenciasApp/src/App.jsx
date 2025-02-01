303dd24f13276ffae4e029baea3ed44bbc680a64
Juan Ignacio Alvarez <alvarez.juanign@gmail.com>
2025-02-01 12:52:44 -0300

    Add toast at add table

--- licenciasApp/src/App.jsx
+++ licenciasApp/src/App.jsx
@@ -25,16 +25,12 @@ export function App() {
   return (
     <main className="flex flex-col items-center p-0 m-0">
       <h1 className="text-2xl font-bold mt-8 print:hidden">Licencias</h1>
-      <div
+      <button
         onClick={manejoAgregar}
-        className="m-8 h-[80px] w-1/2 flex items-center justify-center border-dashed border border-black rounded-md hover:cursor-pointer hover:opacity-45 print:hidden
-        hover:scale-110 duration-300" >
-        <img
-          src={plus}
-          alt="simbolo mas para agregar tabla"
-          title="Agregar tabla"
-          className="group cursor-pointer outline-none scale-125" />
-      </div>
+        className="group m-8 h-[80px] w-1/2 flex items-center justify-center border-dashed border-4 rounded-md hover:cursor-pointer hover:opacity-45 print:hidden
+        active:scale-95 duration-300 active:duration-75 active:outline-none" >
+        <img src={plus} alt="Agregar" className="w-10 h-10 group-hover:opacity-45" />
+      </button>
       <section className="flex flex-col items-center w-screen p-5 print:p-0">
         {tabla.map((tabla, index) => (
           <Tabla key={index} orden={index} />
