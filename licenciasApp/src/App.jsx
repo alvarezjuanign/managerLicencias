b00b2a7c8d98b0d147d5acdd336c81b75c2accb3
Juan Ignacio Alvarez <alvarez.juanign@gmail.com>
2025-02-03 10:55:58 -0300

    Add delete button

--- licenciasApp/src/App.jsx
+++ licenciasApp/src/App.jsx
@@ -17,14 +17,30 @@ export function App() {
     toast.success("Tabla agregada")
   }
 
+  const borrarDatos = () => {
+    localStorage.removeItem("profesores")
+    toast.error("Datos borrados")
+    setTabla([])
+  }
+
   useEffect(() => {
     const profesores = localStorage.getItem("profesores")
     setTabla(profesores ? JSON.parse(profesores) : [])
   }, [])
 
   return (
-    <main className="flex flex-col items-center p-0 m-0">
-      <h1 className="text-2xl font-bold mt-8 print:hidden">Licencias</h1>
+    <main className="flex flex-col items-center p-0 m-0 relative">
+      <div className="flex justify-center items-center gap-12 h-full print:hidden">
+        <div className="bg-gradient-to-b from-stone-300/40 to-transparent p-[4px] rounded-[16px]">
+          <button onClick={borrarDatos} className="group p-[4px] rounded-[12px] bg-gradient-to-b from-white to-stone-200/40 shadow-[0_1px_3px_rgba(0,0,0,0.5)] active:shadow-[0_0px_1px_rgba(0,0,0,0.5)] active:scale-[0.995] absolute top-4 right-4 print:hidden">
+            <div className="bg-gradient-to-b from-stone-200/40 to-white/80 rounded-[8px] px-2 py-2">
+              <div className="flex gap-2 items-center">
+                <span className="font-semibold">Borrar datos</span>
+              </div>
+            </div>
+          </button>
+        </div>
+      </div>
       <button
         onClick={manejoAgregar}
         className="group m-8 h-[80px] w-1/2 flex items-center justify-center border-dashed border-4 rounded-md hover:cursor-pointer hover:opacity-45 print:hidden
@@ -36,7 +52,7 @@ export function App() {
           <Tabla key={index} orden={index} />
         ))}
       </section>
-      <Toaster richColors />
+      <Toaster richColors className="print:hidden" />
     </main>
   )
 }
