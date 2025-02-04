3b6269db167b1ea759ce55a93dbe593eff111bb5
Juan Ignacio Alvarez <alvarez.juanign@gmail.com>
2025-02-04 14:46:40 -0300

    Extract delete button to a component

--- licenciasApp/src/App.jsx
+++ licenciasApp/src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
 import { Tabla } from "./components/Tabla.jsx"
-import plus from "./assets/plus.svg"
 import { Toaster, toast } from "sonner"
+import { Busqueda } from "./components/BarraDeBusqueda.jsx"
+import { BotonBorrar } from "./components/BotonBorrar.jsx"
+import plus from "./assets/plus.svg"
 
 export function App() {
   const [tabla, setTabla] = useState([])
@@ -19,7 +21,6 @@ export function App() {
 
   const borrarDatos = () => {
     localStorage.removeItem("profesores")
-    toast.error("Datos borrados")
     setTabla([])
   }
 
@@ -30,17 +31,8 @@ export function App() {
 
   return (
     <main className="flex flex-col items-center p-0 m-0 relative">
-      <div className="flex justify-center items-center gap-12 h-full print:hidden">
-        <div className="bg-gradient-to-b from-stone-300/40 to-transparent p-[4px] rounded-[16px]">
-          <button onClick={borrarDatos} className="group p-[4px] rounded-[12px] bg-gradient-to-b from-white to-stone-200/40 shadow-[0_1px_3px_rgba(0,0,0,0.5)] active:shadow-[0_0px_1px_rgba(0,0,0,0.5)] active:scale-[0.995] absolute top-4 right-4 print:hidden">
-            <div className="bg-gradient-to-b from-stone-200/40 to-white/80 rounded-[8px] px-2 py-2">
-              <div className="flex gap-2 items-center">
-                <span className="font-semibold">Borrar datos</span>
-              </div>
-            </div>
-          </button>
-        </div>
-      </div>
+      <Busqueda />
+      <BotonBorrar borrar={borrarDatos} />
       <button
         onClick={manejoAgregar}
         className="group m-8 h-[80px] w-1/2 flex items-center justify-center border-dashed border-4 rounded-md hover:cursor-pointer hover:opacity-45 print:hidden
