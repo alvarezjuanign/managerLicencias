1eae8ed50467b75de988c0b0a50a4c20704e6bee
Juan Alvarez <juaniignalvarez@gmail.com>
2025-03-13 14:21:26 -0300

    Refactor search bar layout and improve input styling

--- licenciasApp/src/components/BarraBusqueda.jsx
+++ licenciasApp/src/components/BarraBusqueda.jsx
@@ -19,23 +19,17 @@ export const BarraBusqueda = ({ onSearch }) => {
   };
 
   return (
-    <form className="flex print:hidden">
+    <form className="flex justify-between items-center relative w-64 print:hidden">
       <input
         placeholder="Search..."
-        className="shadow-lg px-5 py-3 rounded-xl w-56 border-2 outline-none"
+        className="focus:outline-none pl-10 pr-4 py-2 rounded-lg border border-gray-200 focus:ring-2 focus:ring-gray-200 focus:border-transparent transition-all"
         name="search"
         type="search"
         value={busqueda}
         onChange={buscar}
         onKeyDown={handleKeyDown}
       />
-      <button
-        type="button"
-        className="shadow-lg px-3 py-2 rounded-xl border-2 outline-none"
-        onClick={() => onSearch(busqueda)}
-      >
-        <img className="w-5 h-5" src={search} alt="Buscar" />
-      </button>
-    </form>
+      <img className="w-5 h-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" src={search} alt="Buscar" />
+    </form >
   );
 };
