472367017c5f33bdf955236e763ab7cb7e3a328f
Juan Alvarez <juaniignalvarez@gmail.com>
2025-03-13 14:21:18 -0300

    Add total property and update button styling

--- licenciasApp/src/App.jsx
+++ licenciasApp/src/App.jsx
@@ -28,7 +28,8 @@ export function App() {
         .map(() => Array(31).fill("")),
       totalParos: 0,
       totalAdministrativas: 0,
-      totalEnfermedad: 0
+      totalEnfermedad: 0,
+      total: 0
     };
     setTabla((prevTabla) => [...prevTabla, tablaProfesor]);
     toast.success("Tabla agregada");
@@ -52,22 +53,23 @@ export function App() {
   };
 
   return (
-    <main className="flex flex-col items-center p-0 m-0 relative">
+    <main className="flex flex-col items-center p-0 m-">
       <nav className="w-full flex justify-evenly m-2">
         <BarraBusqueda onSearch={manejoBusqueda} />
         <BotonBorrar borrar={borrarDatos} />
       </nav>
-      <button
-        onClick={manejoAgregar}
-        className="group m-8 h-[80px] w-1/2 flex items-center justify-center border-dashed border-4 rounded-md hover:cursor-pointer hover:opacity-45 print:hidden
-          active:scale-95 duration-300 active:duration-75 active:outline-none"
-      >
-        <img
-          src={plus}
-          alt="Agregar"
-          className="w-10 h-10 group-hover:opacity-45"
-        />
-      </button>
+      <div className="flex w-96 print:hidden">
+        <button
+          onClick={manejoAgregar}
+          className="w-full max-w-xl border border-dashed border-gray-300 rounded-lg p-4 flex items-center justify-center hover:bg-gray-50 transition-colors"
+        >
+          <img
+            src={plus}
+            alt="Agregar"
+            className="w-10 h-10 group-hover:opacity-45"
+          />
+        </button>
+      </div>
       <section className="flex flex-col items-center w-screen p-5 print:p-0">
         {tablaGuardada.length > 0 ? (
           tablaGuardada.map((_, index) => (
